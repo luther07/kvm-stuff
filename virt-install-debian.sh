349dd7c954522343137acb53ef1e264d308b77b5
Mark Johnson <mark@debian.belkin>
2014-09-21 01:30:32 -0500

    Added virt-install script for debian

---
+++ virt-install-debian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+virt-install  \
+--name=debian \
+--ram=1024 \
+-v \
+--accelerate \
+--nographics \
+--disk path=disk.img,size=4 \
+-l ftp://ftp-nyc.osuosl.org/debian/dists/wheezy/main/installer-amd64/ \
+-w bridge=xenbr0 \
+-x console=ttys0 \
+--prompt
